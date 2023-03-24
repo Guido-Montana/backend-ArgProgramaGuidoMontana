46abf38316f5cef770d7d119b0d2ee16239e1f82
Guido-Montana <guidojuliad@gmail.com>
2023-03-24 18:47:15 -0300

    Primer commit

---
+++ src/main/java/com/portfolio/gjm/GjmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.gjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjmApplication.class, args);
+	}
+
+}
